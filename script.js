37f563831d23da0d351f0f1f6d201d6c2f3a7c87
marshmellow89 <pritamsingh100app@gmail.com>
2025-06-28 02:21:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -10,16 +10,24 @@ document.getElementById("myform").addEventListener("submit",function(event)){
 
 
 
-function submitform(){
-	return new promise((resolve,reject) => {
+
+	const mypromise = new promise((resolve,reject) => {
 		if(age>=18){
 		setTimeout(() => {
-			resolve("Welcome, . You can vote.");
+			resolve(`Welcome,${name}. You can vote.`);
 		},4000);
 		}else{
 			setTimeout(() => {
-				reject("Oh sorry . You aren't old enough.");
+				reject(`Oh sorry ${name}. You aren't old enough.`);
 			});
 		};
 	});
-}
+
+mypromise
+     .then((data1) => {
+		 console.log(`data1`);
+	 })
+    .catch((data2) => {
+		console.log(`data2`);
+	});
+
