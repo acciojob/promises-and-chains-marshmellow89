63d7701ac65a5d936150d92c93d62ccca1e1f920
marshmellow89 <pritamsingh100app@gmail.com>
2025-06-28 01:51:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
-//your JS code here. If required.
+const name = document.getElementById("name").value.trim();
+const age = document.getElementById("age").value.trim();
+
+if(!name || !age){
+	alert("Please enter valid details.");
+	document.getElementById("myform").addEventListener("submit",event.preventDefault());
+}else{
+function submitform(){
+	return new promise((resolve,reject) => {
+		if(age>=18){
+		setTimeout(() => {
+			resolve("Welcome, . You can vote.");
+		},4000);
+		}else{
+			setTimeout(() => {
+				reject("Oh sorry . You aren't old enough.");
+			});
+		};
+	});
+}
+};
